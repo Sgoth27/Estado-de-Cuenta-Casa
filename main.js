5875719a39fc997efac63491f96dbd7fd268f6a4
Santiago Triana <sgoth27@gmail.com>
2023-02-25 16:19:46 -0500

    adicion de valor total

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const table = document.querySelector('#mainTable');
 const mainTableBody = document.querySelector('#mainTableBody');
 const trbody = document.querySelector('#mainTrbody');
+const totalValor = document.querySelector('#totalValor');
+const tdFormatoNumero = document.querySelector('#formatoNumero');
 
 const gastos = [
     //registro:[],
@@ -8,6 +10,8 @@ const gastos = [
 ];
 
 let id = 0;
+
+const gastosArray = [] ;
 
 function crearID(){
         
@@ -28,6 +32,8 @@ function crearID(){
 }
 
 function meterDatos(fecha,gasto,valor,persona){
+
+    
     crearID();
     if(!gastos[id-1].registro){
         gastos[id-1].registro = [];
@@ -36,13 +42,41 @@ function meterDatos(fecha,gasto,valor,persona){
     }
     gastos[id-1].registro.push(fecha);
     gastos[id-1].registro.push(gasto);
+
+    // const valorLista = Number(valor);
+    // console.log('Este es:' + valorLista);
+    // const formato1 = valorLista.toLocaleString("en", {
+    //     style: "decimal",
+    //     currency: "cop",
+    // });
+    // console.log(formato1);
+
     gastos[id-1].registro.push(valor);
     gastos[id-1].registro.push(persona);
     
     ingresarAlDom(gastos[id-1].registro);
+
+   
+    gastosArray.push(parseInt(gastos[id-1].registro[2]));
+    console.log( gastosArray);
+    sumaGastos(gastosArray);
+    //cambiarFormato(gastosArray);
+
+    
     return gastos;
 }
 
+// function cambiarFormato(lista){
+//     console.log('esto es' +lista);
+//     for(elemento of lista){
+//         elemento = elemento.toLocaleString("es", {
+//             style: "decimal",
+//             currency: "cop",
+//         });
+//         console.log(elemento);
+
+//     }
+// }
 
 
 function ingresarAlDom(lista){
@@ -51,6 +85,8 @@ function ingresarAlDom(lista){
 
     for(let i=0; i<lista.length; i++){
         let td = document.createElement('td');
+        
+        
         td.innerText = lista[i];
         tr.appendChild(td);
         
@@ -62,14 +98,26 @@ function ingresarAlDom(lista){
 
 }
 
-meterDatos('05-02-23','almuerzos','$50.000','NT');
-meterDatos('05-02-23','pedialite','$7.500','NT');
-meterDatos('12-02-23','almuerzos','$11.500','NT');
-meterDatos('12-02-23','luz','$418.100','JE');
-meterDatos('15-02-23','15na Estela','$604.000','ST');
-meterDatos('15-02-23','claro','$81.900','JE');
-meterDatos('15-02-23','claro','$72.695','ST');
-meterDatos('18-02-23','almuerzo','$13.300','NT');
-meterDatos('18-02-23','etb','$165.000','ST');
-meterDatos('19-02-23','almuerzos','$40.000','NT');
+function sumaGastos(lista){
+    const sumaValores = lista.reduce((a,b) => a + b);
+    console.log(sumaValores);
+    const formatoNumero = sumaValores.toLocaleString("es", {
+        style: "decimal",
+        currency: "cop",
+    });
+    console.log(formatoNumero);
+    totalValor.innerText = `$${formatoNumero}`;
+       
+}
+
+meterDatos('05-02-23','almuerzos','50000','NT');
+meterDatos('05-02-23','pedialite','7500','NT');
+meterDatos('12-02-23','almuerzos','11500','NT');
+meterDatos('12-02-23','luz','418100','JE');
+meterDatos('15-02-23','15na Estela','604000','ST');
+meterDatos('15-02-23','claro','81900','JE');
+meterDatos('15-02-23','claro','72695','ST');
+meterDatos('18-02-23','almuerzo','13300','NT');
+meterDatos('18-02-23','etb','165000','ST');
+meterDatos('19-02-23','almuerzos','40000','NT');
 
